1f21abf7569445ff04e84125d02bc3a5bbc0bb87
Guilherme Carrera <guiacc26@gmail.com>
2025-06-10 15:18:25 -0300

    Estabelicido conexão com o banco, e migrations

---
+++ src/database/migrations/20250610180854_create_inscricoes_table.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+    return knex.schema.createTable('inscricoes', function (table) {
+        table.increments('id').primary();
+        table.integer('id_participante').unsigned().notNullable()
+            .references('id').inTable('participantes').onDelete('CASCADE');
+        table.integer('id_evento').unsigned().notNullable()
+            .references('id').inTable('eventos').onDelete('CASCADE');
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.unique(['id_participante', 'id_evento']); // Garantir que um participante só possa se inscrever uma vez em um evento
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+    return knex.schema.dropTable('inscricoes');
+};
---
+++ src/routes/eventos.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const eventoController = require('../controllers/eventoController');
+
+router.get('/', eventoController.index);
+router.post('/', eventoController.store);
+
+module.exports = router;
