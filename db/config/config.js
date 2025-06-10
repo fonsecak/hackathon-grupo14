9bf4fbee220ff2b96e6c04bc246bd2e00265bf4b
Guilherme Carrera <guiacc26@gmail.com>
2025-06-10 10:56:49 -0300

    conexão com o banco de dados com sequelize CLI criada

---
+++ db/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": "",
+    "database": "alfa_experience_db",
+    "host": "localhost",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": "",
+    "database": "alfa_experience_db",
+    "host": "localhost",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": "",
+    "database": "alfa_experience_db",
+    "host": "localhost",
+    "dialect": "mysql"
+  }
+}
