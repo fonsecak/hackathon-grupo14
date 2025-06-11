03a220e68113ffcfc8d6d50fe944f43b68f28c27
prozzin <brayanadriano456@gmail.com>
2025-06-10 21:23:35 -0300

    Realizada a inicialização da conexão com o db

--- back-office/cadastros/src/main/java/com/alfa/experience/dao/Dao.java
+++ back-office/cadastros/src/main/java/com/alfa/experience/dao/Dao.java
@@ -1,4 +1,29 @@
 package com.alfa.experience.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Dao {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/alfa_experience_db?useTimezone=true&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    private Connection connection;
+
+    public Dao() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            this.connection = DriverManager
+                    .getConnection(URL, USER, PASSWORD);
+        } catch (Exception e) {
+            System.out.println("[DAO Connection] " + e.getMessage());
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+
 }
