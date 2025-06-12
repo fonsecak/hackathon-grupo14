df288cd149052e6ac4480918d1ac1d8052cafd01
prozzin <brayanadriano456@gmail.com>
2025-06-12 20:33:10 -0300

    Alterado HTML

--- resources/views/eventos.php
+++ resources/views/eventos.php
@@ -1,6 +1,6 @@
-
 <body class="bg-light">
 
+  <!-- Seção: Eventos em destaque -->
   <div class="container py-5">
     <h2 class="mb-4 text-left">Eventos em destaque</h2>
 
@@ -16,7 +16,6 @@
             <a href="#" class="btn btn-primary w-100">Inscreva-se</a>
           </div>
         </div>
-        
       </div>
 
       <div class="col-md-4 col-lg-4">
@@ -30,10 +29,9 @@
             <a href="#" class="btn btn-primary w-100">Inscreva-se</a>
           </div>
         </div>
-        
       </div>
 
- <div class="col-md-4 col-lg-4">
+      <div class="col-md-4 col-lg-4">
         <div class="card event-card shadow-sm">
           <img src="../img/eventoteste.png" class="card-img-top" alt="Imagem do evento">
           <div class="card-body">
@@ -44,24 +42,15 @@
             <a href="#" class="btn btn-primary w-100">Inscreva-se</a>
           </div>
         </div>
-
+      </div>
     </div>
-    
   </div>
 
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-
-
-<body class="bg-light py-5">
-
-  <div class="container">
+  <!-- Seção: Categorias dos cursos -->
+  <div class="container py-5">
     <div class="d-flex justify-content-between align-items-center mb-4">
-      <h2 class="mb-4 text-left">Explore nossos cursos.</h2>
-      <a href="#" class="text-primary text-decoration-none fw-semibold">Ver tudo</a>
+      <h2 class="mb-0">Explore nossos cursos.</h2>
+      
     </div>
 
     <div class="row g-3">
@@ -74,7 +63,7 @@
       <div class="col-6 col-sm-4 col-md-3 col-lg-2">
         <button class="btn btn-light w-100 categoria-btn">
           <i class="bi bi-fire fs-4 mb-1"></i>
-          <div class="small">Cieências Contabeis</div>
+          <div class="small">Ciências Contábeis</div>
         </button>
       </div>
       <div class="col-6 col-sm-4 col-md-3 col-lg-2">
@@ -103,11 +92,6 @@
       </div>
     </div>
   </div>
-
-
-
-
-
 
   <!-- Bootstrap JS opcional -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
